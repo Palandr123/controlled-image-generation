5147050cbc2cd434b7fe5e8a8c6d7373deb4036a
Andrey Palaev <a.palaev@innopolis.university>
2023-03-19 19:13:43 +0000

    Fix types and add __init__

--- gans/gan_wrapper.py
+++ gans/gan_wrapper.py
@@ -3,7 +3,10 @@ import torch.nn as nn
 
 
 class GanWrapper(nn.Module):
-    def forward(self, z: torch.FloatTensor) -> torch.FloatTensor:
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__()
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
         """
         forward method for GAN
         :param z: latent vector, torch.tensor of shape (N, z_dim)
--- gans/stylegan.py
+++ gans/stylegan.py
@@ -8,9 +8,10 @@ from gans.gan_wrapper import GanWrapper
 
 class StyleGAN(GanWrapper):
     def __init__(self, weights_path: Path, device: str) -> None:
+        super().__init__()
         self.device = torch.device(device)
         with open(str(weights_path), 'rb') as f:
             self.G = pickle.load(f)['G_ema'].cuda()
 
-    def forward(self, z: torch.FloatTensor) -> torch.FloatTensor:
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
         return self.G(z, None)
