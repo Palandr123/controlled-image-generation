7374d6fecf1c5003003af222b93f22704d3ac9e1
Andrey Palaev <a.palaev@innopolis.university>
2023-03-20 23:00:49 +0000

    Modify embedder

--- models/embedder.py
+++ models/embedder.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torchvision.models import resnet18
 
 from utils import create_mlp
 
@@ -7,19 +8,22 @@ from utils import create_mlp
 class Embedder(nn.Module):
     """
     class for embedding the transformations
-    @:param self.net - multi layer perceptron for that produces the embeddings for the transformations
+    @:param self
+    @:param self.features_extractor - ResNet-based network that produces the embeddings for the transformations
     """
-    def __init__(self, n_layers: int, in_features: int, middle_features: int, out_features: int) -> None:
+    def __init__(self, n_layers: int, middle_features: int, out_features: int) -> None:
         super().__init__()
-        self.net = create_mlp(n_layers, in_features, middle_features, out_features)
+        self.feature_extractor = resnet18(pretrained=True)
+        self.features_extractor.conv1 = nn.Conv2d(6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        in_features = self.features_extractor.layer4[1].conv2.out_channels
+        self.features_extractor.fc = create_mlp(n_layers, in_features, middle_features, out_features)
 
-    def forward(self, z1: torch.Tensor, z2: torch.Tensor) -> torch.Tensor:
+    def forward(self, img1: torch.Tensor, img2: torch.Tensor) -> torch.Tensor:
         """
         forward method for embedder
-        :param z1: batch of latent vectors, torch.tensor of shape (N, z_dim)
-        :param z2: batch of latent vectors that are results of the manipulation on latent vectors z1
+        :param img1: batch of images, torch.tensor of shape (N, z_dim)
+        :param img2: batch of images that are results of the manipulation on latent vectors corresponding to img1
         :return: embedding for the manipulations, of the shape (N, out_features)
         """
-        z = torch.cat([z1, z2], dim=1)
-        out = self.net(z)
-        return out / torch.norm(out, dim=1)
+        return self.features_extractor(torch.cat([img1, img2], dim=1)).reshape(img1.shape(0), -1)
+
