332b8219a78f5303a424e4ca0af26915ff21c990
Andrey Palaev <a.palaev@innopolis.university>
2023-03-20 22:44:36 +0000

    Add z_dim attribute to StyleGAN class

--- gans/stylegan.py
+++ gans/stylegan.py
@@ -12,6 +12,7 @@ class StyleGAN(GanWrapper):
         self.device = torch.device(device)
         with open(str(weights_path), 'rb') as f:
             self.G = pickle.load(f)['G_ema'].cuda()
+        self.z_dim = self.G.z_dim
 
     def forward(self, z: torch.Tensor) -> torch.Tensor:
         return self.G(z, None)
