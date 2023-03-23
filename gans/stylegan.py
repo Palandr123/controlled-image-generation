15b152d5b0fb440184aef7d4d56ae5c056f77a50
Andrey Palaev <a.palaev@innopolis.university>
2023-03-23 10:55:49 +0000

    remove device from StyleGAN class

--- gans/stylegan.py
+++ gans/stylegan.py
@@ -6,11 +6,10 @@ from gans.gan_wrapper import GanWrapper
 
 
 class StyleGAN(GanWrapper):
-    def __init__(self, weights_path: str, device: str) -> None:
+    def __init__(self, weights_path: str) -> None:
         super().__init__()
-        self.device = torch.device(device)
         with open(weights_path, 'rb') as f:
-            self.G = pickle.load(f)['G_ema'].cuda()
+            self.G = pickle.load(f)['G_ema']
         self.z_dim = self.G.z_dim
 
     def forward(self, z: torch.Tensor) -> torch.Tensor:
