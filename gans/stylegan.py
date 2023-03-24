84c295a4c2535d6def48309dc717072ec4afddd3
Andrey Palaev <a.palaev@innopolis.university>
2023-03-24 18:59:44 +0000

    Add get_features() and sample_latent() methods

--- gans/stylegan.py
+++ gans/stylegan.py
@@ -14,3 +14,17 @@ class StyleGAN(GanWrapper):
 
     def forward(self, z: torch.Tensor) -> torch.Tensor:
         return self.G(z, None)
+
+    def get_features(self, z: torch.Tensor) -> torch.Tensor:
+        w = self.G.mapping(z, None)
+        x = img = None
+        layers = list(self.G.synthesis.children())
+        idx = 0
+        for i, layer in enumerate(layers[:7]):
+            num = layer.num_conv + layer.num_torgb
+            x, img = layer(x, img, w[:, idx:idx+num])
+            idx = idx + layer.num_conv
+        return img
+
+    def sample_latent(self, batch_size: int, device: torch.device) -> torch.Tensor:
+        return torch.randn([batch_size, self.z_dim]).to(device)
